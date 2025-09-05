24c3a7809800252f0543450b73ea971b78cdece0
Gajendra Sharma <gajendrasharma0145@gmail.com>
2025-09-05 21:15:39 +0530

    refactor: separate app initialization and UI from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:driver_app/config/firebase_config.dart';
-import 'package:driver_app/screens/home_screen.dart';
+import 'app_initializer.dart';
+import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize Firebase
-  await FirebaseConfig.initialize();
+  await AppInitializer.initialize(); 
 
   runApp(const ProviderScope(child: BusTrackerApp()));
-}
-
-class BusTrackerApp extends StatelessWidget {
-  const BusTrackerApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bus Tracker',
-      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Roboto'),
-      home: const HomeScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
 }
